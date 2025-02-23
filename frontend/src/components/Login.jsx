1332a76c59cc7947d2b92a7b281a9a36698a3b48
Mehak Narang <139652649+mehak-World@users.noreply.github.com>
2025-02-23 14:13:24 -0800

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -42,7 +42,7 @@ const Login = () => {
         setAuthHeader(response.data.token);
         window.localStorage.setItem("username", username);
         setUserName(username);
-        navigate("/");
+        navigate("/login");
       }
 
     
